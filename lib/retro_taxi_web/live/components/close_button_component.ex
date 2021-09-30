5ed3190e2fe5fd165e87f356667dbe17277ecc66
Mike Zornek <zorn@zornlabs.com>
2021-09-30 14:01:46 -0400

    Since this component can be used to cancel/close a compose form, we will make the confirmation message more generic.

--- lib/retro_taxi_web/live/components/close_button_component.ex
+++ lib/retro_taxi_web/live/components/close_button_component.ex
@@ -3,7 +3,7 @@ defmodule RetroTaxiWeb.CloseButtonComponent do
 
   def render(assigns) do
     ~L"""
-    <button type="button" data-confirm="Are you sure you want to revert this edit?" class="w-6 h-6" phx-click="<%= @click_event %>" phx-target=<%= @target %>>
+    <button type="button" data-confirm="Are you sure you want to cancel?" class="w-6 h-6" phx-click="<%= @click_event %>" phx-target=<%= @target %>>
       <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-700 hover:text-gray-800" fill="none"
         viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
